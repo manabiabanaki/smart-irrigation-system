458944c45727fd72f8198942165dc82c202a4f05
manabiabanaki <biabanaki.eng@gmail.com>
2025-11-30 12:30:30 +0330

    Update visualize.py

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,1 +1,9 @@
+import matplotlib.pyplot as plt
 
+def plot_results(model, data):
+    X = data[['soil_moisture', 'temperature', 'humidity']]
+    y_pred = model.predict(X)
+    plt.plot(data.index, y_pred, label='Predicted water')
+    plt.plot(data.index, data['water_requirement'], label='Actual water')
+    plt.legend()
+    plt.show()
