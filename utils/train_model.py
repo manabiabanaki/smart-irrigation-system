a27b0a2e7c1ad20f9ffc5bf95f3a5f17e26f574a
manabiabanaki <biabanaki.eng@gmail.com>
2025-11-30 12:30:00 +0330

    Update train_model.py

--- utils/train_model.py
+++ utils/train_model.py
@@ -1,1 +1,8 @@
+from sklearn.linear_model import LinearRegression
 
+def train_model(data):
+    X = data[['soil_moisture', 'temperature', 'humidity']]
+    y = data['water_requirement']
+    model = LinearRegression()
+    model.fit(X, y)
+    return model
